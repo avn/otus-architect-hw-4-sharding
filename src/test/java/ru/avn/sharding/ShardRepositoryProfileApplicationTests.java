676f725afc8a40f414ba6384851b432380300182
avn <avnikulin.niiaa@gmail.com>
2019-12-27 19:00:36 +0300

    tests were fixed

--- src/test/java/ru/avn/sharding/ShardRepositoryProfileApplicationTests.java
+++ src/test/java/ru/avn/sharding/ShardRepositoryProfileApplicationTests.java
@@ -3,9 +3,11 @@ package ru.avn.sharding;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles(ProfileName.SHARD_REPOSITORY)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
 class ShardRepositoryProfileApplicationTests {
 
     @Test
